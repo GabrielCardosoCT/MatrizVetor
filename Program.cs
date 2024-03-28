6b71ad60f213f8eb43273e3747c9beb1b762a39b
Gabriel Cardoso <gabrielfaria00@hotmail.com>
2024-03-28 19:58:08 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+    {
+    static void Main (string[] args)
+        {
+
+        int soma = 0;
+        
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"Valor {i}: ");
+            int valor = int.Parse(Console.ReadLine());
+
+            soma += valor;
+        }
+        
+        Console.WriteLine();
+
+        int media = soma / 10;
+        Console.WriteLine(media);
+        }
+    }
